5899e2a43129ec2c2cfae99d262cdb19234c0a53
dotnotation <dotinmotionproductions@gmail.com>
2022-04-16 14:48:47 -0400

    completed and passed tests

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+  return (
+    <div className='eyesOnMe'>
+        <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    </div>
+  )
+}
